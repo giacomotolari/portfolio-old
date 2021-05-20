b3d507f61a0d1dbd122000038ad746abf4d2080e
giacomotolari <tolarigiacomo@gmail.com>
2021-05-20 14:56:19 +0200

    fiy styles

--- src/components/main/skills/Skills.jsx
+++ src/components/main/skills/Skills.jsx
@@ -11,7 +11,36 @@ const Skills = () => {
       <div className='skillsLogo javaScriptLogo'></div>
       <div className='skillsLogo htmlLogo'></div>
       <div className='skillsLogo cssLogo'></div>
+      <div className='skillsLogo linuxLogo'></div>
+      <div className='skillsLogo windowsLogo'></div>
+      <div className='skillsLogo vsLogo'></div>
+      <div className='skillsLogo gimpfLogo'></div>
       <div className='brain'></div>
+      <section className='skillsLists'>
+        
+        <ul className='informatic'>
+        <h4>Informatic</h4>
+          <li>Linux / Ubuntu</li>
+          <li>Windows / Android</li>
+          <li>Git / GitHUb</li>
+          <li>Terminal / Bash</li>
+          <li>Gimp</li>
+          <li>Visual Studio Code</li>
+        </ul>
+        <ul className='frontEnd'>
+        <h4>Front End</h4>
+          <li>HTML5</li>
+          <li>CSS3 / SASS / BOOTSTRAP</li>
+          <li>JavaSript / REACT JS</li> 
+        </ul>
+        <ul className='languages'>
+        <h4>Languages</h4>
+          <li>Italian</li>
+          <li>German</li>
+          <li>English</li>
+          <li>Spanish</li>
+        </ul>
+      </section>
     </div>
   );
 };
