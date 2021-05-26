fd583b63311429e0e58a7a3cb641a92aad5fb748
giacomotolari <tolarigiacomo@gmail.com>
2021-05-26 12:02:25 +0200

    create project

--- src/components/main/projects/Projects.jsx
+++ src/components/main/projects/Projects.jsx
@@ -5,6 +5,24 @@ const Projects = () => {
   return (
     <div className='Projects'>
       <div className='spaceApparition'></div>
+      <section className='projectsWrapper'>
+        <div className='project'>
+          <h5>Poems</h5>
+          <div className='projectBackground project1'></div>
+          <div className='projectIcons'></div>
+        </div>
+
+        <div className='project'>
+          <h5>Coach</h5>
+          <div className='projectBackground project2'></div>
+          <div className='projectIcons'></div>
+        </div>
+        <div className='project'>
+          <h5>Project-Owner</h5>
+          <div className='projectBackground project3'></div>
+          <div className='projectIcons'></div>
+        </div>
+      </section>
     </div>
   );
 };
