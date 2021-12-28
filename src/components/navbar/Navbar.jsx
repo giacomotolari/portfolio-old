a76c06314374eeb514b3bc30d8eab343f4fbe111
giacomotolari <tolarigiacomo@gmail.com>
2021-12-28 22:26:00 +0100

    burgerMenu fix bug with navbar Functions

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,56 +8,61 @@ const Navbar = () => {
   const { burgerMenu, setBurgerMenu, devices } = useTheme();
 
   const burgerMenuToggle = () => {
-    if (burgerMenu === false) {
+    if (devices.burgerMenu && burgerMenu === false) {
       setBurgerMenu(true);
     }
-    if (burgerMenu === true) {
+    if (devices.burgerMenu && burgerMenu === true) {
       setBurgerMenu(false);
     }
   };
 
   return (
     <div className='Navbar'>
-      {!devices.burgerMenu ||
-        burgerMenu === true ? (
-          <ul className={!burgerMenu ? 'navBar' : 'burgerMenu'}>
-            <li onClick={burgerMenuToggle}>
-              <NavLink id='navHome' className='navLink' to='/'>
-                HOME
-              </NavLink>
-            </li>
-            <li onClick={burgerMenuToggle}>
-              <NavLink id='navProjects' className='navLink' to='/projects'>
-                PROJECTS
-              </NavLink>
-            </li>
-            <li onClick={burgerMenuToggle}>
-              <NavLink id='navAbout' className='navLink' to='/about'>
-                ABOUT
-              </NavLink>
-            </li>
+      {!devices.burgerMenu || burgerMenu === true ? (
+        <ul className={!burgerMenu ? 'navBar' : 'burgerMenu'}>
+          <li onClick={burgerMenuToggle}>
+            <NavLink id='navHome' className='navLink' to='/'>
+              HOME
+            </NavLink>
+          </li>
+          <li onClick={burgerMenuToggle}>
+            <NavLink id='navProjects' className='navLink' to='/projects'>
+              PROJECTS
+            </NavLink>
+          </li>
+          <li onClick={burgerMenuToggle}>
+            <NavLink id='navAbout' className='navLink' to='/about'>
+              ABOUT
+            </NavLink>
+          </li>
 
-            <li onClick={burgerMenuToggle}>
-              <NavLink id='navContact' className='navLink' to='/contact'>
-                CONTACT
-              </NavLink>
-            </li>
-          </ul>
-        ) : 
-      devices.burgerMenu && !burgerMenu === true && (
-        <div onClick={burgerMenuToggle} className='burgerIcon' to='/burgermenu'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='currentColor'
-            className='bi bi-list'
-            viewBox='0 0 16 16'
+          <li onClick={burgerMenuToggle}>
+            <NavLink id='navContact' className='navLink' to='/contact'>
+              CONTACT
+            </NavLink>
+          </li>
+        </ul>
+      ) : (
+        devices.burgerMenu &&
+        !burgerMenu === true && (
+          <div
+            onClick={burgerMenuToggle}
+            className='burgerIcon'
+            to='/burgermenu'
           >
-            <path
-              fillRule='evenodd'
-              d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
-            />
-          </svg>
-        </div>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='currentColor'
+              className='bi bi-list'
+              viewBox='0 0 16 16'
+            >
+              <path
+                fillRule='evenodd'
+                d='M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z'
+              />
+            </svg>
+          </div>
+        )
       )}
       {devices.burgerMenu && burgerMenu === true && (
         <p className='burgerIcon' onClick={burgerMenuToggle}>
