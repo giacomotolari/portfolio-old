9007ad9626b6d953fa04bcd6a9fbdd0a92c167ed
giacomotolari <tolarigiacomo@gmail.com>
2021-12-30 15:00:45 +0100

    add home animation

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,26 +1,96 @@
 /* eslint-disable import/no-extraneous-dependencies */
-
 import React from 'react';
+import brain from '../../asset/images/brain2.gif';
+import react from '../../asset/images/logos/react.png';
+import css from '../../asset/images/logos/css.png';
+import sass from '../../asset/images/logos/sass.png';
+// import giacomo from '../../asset/images/giacomo.mp4'
 
 const Home = () => {
   return (
     <div className='Home'>
-      <section className='sectionText'>
-        <div class='home-text'>
-          <p>
-            Hello everyone! <br />
-            <br />
-            <br /> Giacomo is there, I am a Junior Full Stack Developer
-            <br />
-            In this page I'll introduce myself, my skills and my projects.
-          </p>
-        </div>
-      </section>
+      
+      <div className='wrap' id='wrap'>
+        <div className='left'>
+          <div
+            className='chouchin-wrap chouchin1 chouchin layer1'
+            data-speed-x='40'
+            data-speed-y='20'
+          >
+            <img src={brain} alt='...' className='chouchin' />
+          </div>
 
+          <div
+            className='chouchin-wrap chouchin2 chouchin layer2'
+            data-speed-x='20'
+            data-speed-y='10'
+          >
+            <img src={react} alt='...' className='chouchin' />
+          </div>
+
+          <div
+            className='chouchin-wrap chouchin3 chouchin layer3'
+            data-speed-x='10'
+            data-speed-y='5'
+          >
+            <img src={css} alt='...' className='chouchin' />
+          </div>
+        </div>
+
+        <div className='right'>
+          <div
+            className='chouchin-wrap chouchin4 chouchin layer1'
+            data-speed-x='40'
+            data-speed-y='20'
+          >
+            <img src={sass} alt='...' className='chouchin' />
+          </div>
+
+          <div
+            className='chouchin-wrap chouchin5 chouchin layer2'
+            data-speed-x='20'
+            data-speed-y='10'
+          >
+            <img
+              src='https://1.bp.blogspot.com/-cEhj3_MMKi4/XQjuNuurZLI/AAAAAAABTQA/Rv_ZRt-6O0EB1sfJiLki4MXA-nOlf-DzQCLcBGAs/s800/kingyo_chouchin.png'
+              alt='...'
+              className='chouchin'
+            />
+          </div>
+
+          <div
+            className='chouchin-wrap chouchin6 chouchin layer3'
+            data-speed-x='10'
+            data-speed-y='5'
+          >
+            <img
+              src='https://1.bp.blogspot.com/-cEhj3_MMKi4/XQjuNuurZLI/AAAAAAABTQA/Rv_ZRt-6O0EB1sfJiLki4MXA-nOlf-DzQCLcBGAs/s800/kingyo_chouchin.png'
+              alt='...'
+              className='chouchin'
+            />
+          </div>
+        </div>
+
+       
+      </div>
+
+      <div className='content'>
+        <div className='content__container'>
+          <p className='content__container__text'>I'm</p>
+
+          <ul className='content__container__list'>
+            <li className='content__container__list__item'>Giacomo</li>
+            <li className='content__container__list__item'>a web developer</li>
+            <li className='content__container__list__item'>full stack</li>
+            <li className='content__container__list__item'>who I am!</li>
+          </ul>
+        </div>
+      </div>
       <section className='sectionFoto'>
         <h1>GIACOMO TOLARI</h1>
 
-        <div className='profilefoto'></div>
+        {/* <div className='profilefoto'></div> */}
+        {/* <video img={giacomo} className='profilefoto' alt='giacomo'/> */}
       </section>
     </div>
   );
--- src/components/main/about/About.jsx
+++ src/components/main/about/About.jsx
@@ -1,26 +1,61 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import giacomoDaniela from '../../../asset/images/giacomoDaniela.jpg';
+import { NavLink } from 'react-router-dom';
 
 const About = () => {
   return (
     <div className='About'>
-     <img src={giacomoDaniela} className='demo-bg' alt='giacomokayak' />
+      <img src={giacomoDaniela} className='demo-bg' alt='giacomokayak' />
       <div className='spaceApparitionAbout'></div>
       <div class='about-text'>
-        <p>
-          I am a 30 year old guy from Italy (Sardinia), I love sport, music and
-          cooking (more eating).
-        </p>
-        <br />
-        <p>
+
+      <p>
           I like the idea of being able to work with people from all over the
           world, peole of different professions, knowing that I will never stop
           learning and that I will be able to use my imagination in my work,
           which is why I chose to become a Web Developer.
         </p>
+        
+        <p>
+          I am a 30 year old guy from Italy (Sardinia), I love sport, music and
+          cooking (more eating).
+        </p>
+
+        
+        <p>
+          I started my career at school at the age of 15, where I learned the
+          basics of HTML. In 2020 I started studying again with courses on Udemy
+          and Codecademy then on 01.03.2021 I started the Full Stack Web
+          Developer school at DCI (Digital Career Institut) which will end on
+          28.02.22.
+        </p>
+        <p>
+          Most of the practice I have done is building web pages for friends and
+          relatives that will serve them for their business and private
+          purposes. The pages are still under construction, every time I learn
+          something new I try to put it into these{' '}
+          <NavLink className='navLink' to='/projects'>
+            pages
+          </NavLink>
+          and make them better and better.
+        </p>
+        <p>
+          Visit my
+          <a
+            className='navLink'
+            href='https://www.linkedin.com/in/giacomo-tolari/'
+            target='blank'
+          >
+            linkedin
+          </a>
+          profile for more details or just{' '}
+          <NavLink className='navLink' to='/contact'>
+            contact
+          </NavLink>
+          me
+        </p>
       </div>
-      {/* <img src={giacomokayak} className='foto' alt='giacomokayak' /> */}
     </div>
   );
 };
